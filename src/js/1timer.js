34719163bc164374662b880ef8b1aef41ae80b8f
VovanOdessa1991 <50461900+VovanOdessa1991@users.noreply.github.com>
2024-06-11 10:41:27 +0200

    debug_Input:focus

--- src/js/1timer.js
+++ src/js/1timer.js
@@ -3,7 +3,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 // Описаний у документації
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
+import 'izitoast/dist/css/iziToast.css';
 // datetime - picker;
 let userDate;
 // let novDate;
